4649c11214136c29fa8214dce95c81f8f049d520
Dani <rozsadani91@gmail.com>
2016-09-14 22:30:31 +0200

    Fragment layout inflater parent attach fix.

--- app/src/main/java/hu/daniel/rozsa/fragment/StartScreenFragment.java
+++ app/src/main/java/hu/daniel/rozsa/fragment/StartScreenFragment.java
@@ -19,6 +19,6 @@ public class StartScreenFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_start_screen, container, true);
+        return inflater.inflate(R.layout.fragment_start_screen, container, false);
     }
 }
